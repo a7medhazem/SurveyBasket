9c13284830096c86e1a04b94c021ef79ed5e287e
Ahmed Hazem <155444633+a7medhazem@users.noreply.github.com>
2026-01-21 17:06:32 +0200

    Implement refresh token and revoke token endpoints

---
+++ SurveyBasket.Api/Contracts/Authentication/RefreshTokenRequestValidator.cs
@@ -0,0 +1,10 @@
+﻿namespace SurveyBasket.Api.Contracts.Authentication;
+
+public class RefreshTokenRequestValidator : AbstractValidator<RefreshTokenRequest>
+{
+    public RefreshTokenRequestValidator()
+    {
+        RuleFor(x => x.token).NotEmpty();
+        RuleFor(x => x.refreshToken).NotEmpty();
+    }
+}
