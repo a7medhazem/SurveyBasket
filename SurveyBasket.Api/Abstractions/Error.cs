e0fe08ed6ec2401a9a0ead4fbbaaeaea42b81ce5
Ahmed Hazem <155444633+a7medhazem@users.noreply.github.com>
2026-01-28 23:03:29 +0200

    Add Result and Error classes for success and failure handling.

---
+++ SurveyBasket.Api/Abstractions/Error.cs
@@ -0,0 +1,7 @@
+﻿namespace SurveyBasket.Api.Abstractions;
+
+public record Error(string Code, string Description)
+{
+    public static readonly Error None = new(string.Empty, string.Empty);// Represents a successful operation with no error
+
+}
