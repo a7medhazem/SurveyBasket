cbb8d5de517ecba9a6e11375da1bc57a28a45c75
Ahmed Hazem <155444633+a7medhazem@users.noreply.github.com>
2026-02-10 23:59:30 +0200

    Refactor QuestionRequestValidator to add new rules

--- SurveyBasket.Api/Contracts/Questions/QuestionRequestValidator.cs
+++ SurveyBasket.Api/Contracts/Questions/QuestionRequestValidator.cs
@@ -4,17 +4,23 @@ public class QuestionRequestValidator : AbstractValidator<QuestionRequest>
 {
     public QuestionRequestValidator()
     {
-        RuleFor(x => x.Content)
-            .NotEmpty()
-            .Length(3, 1000);
+      RuleFor(x => x.Content)
+      .NotEmpty()
+      .Length(3, 1000);
 
-        RuleFor(x => x.Answers)
-            .Must(x => x.Count > 1)
-            .WithMessage("Question should has at least 2 answers");
+      RuleFor(x => x.Answers)
+     .NotNull();
 
-        RuleFor(x => x.Answers)
-            .Must(x => x.Distinct().Count() == x.Count)
-            .WithMessage("you can't add duplicated answers for the same qustion");
+      RuleFor(x => x.Answers)
+     .Must(x => x.Count > 1)
+     .WithMessage("Question should has at least 2 answers")
+     .When(x => x.Answers != null);
+
+     RuleFor(x => x.Answers)
+     .Must(x => x.Distinct().Count() == x.Count)
+     .WithMessage("you can't add duplicated answers for the same qustion")
+     .When(x => x.Answers != null);
+
 
     }
 }
