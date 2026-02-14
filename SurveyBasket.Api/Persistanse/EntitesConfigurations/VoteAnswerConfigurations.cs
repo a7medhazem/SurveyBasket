7e7abeb4ee12549ae4600621698701a4e3e08251
Ahmed Hazem <155444633+a7medhazem@users.noreply.github.com>
2026-02-14 23:44:48 +0200

    Implement poll voting feature with Vote and VoteAnswer entities, EF Core configurations and database migration.

---
+++ SurveyBasket.Api/Persistanse/EntitesConfigurations/VoteAnswerConfigurations.cs
@@ -0,0 +1,10 @@
+﻿namespace SurveyBasket.Api.Persistanse.EntitesConfigurations;
+
+public class VoteAnswerConfigurations : IEntityTypeConfiguration<VoteAnswer>
+{
+    public void Configure(EntityTypeBuilder<VoteAnswer> builder)
+    {
+        builder.HasIndex(x => new { x.VoteId, x.QuestionId }).IsUnique();
+
+    }
+}
