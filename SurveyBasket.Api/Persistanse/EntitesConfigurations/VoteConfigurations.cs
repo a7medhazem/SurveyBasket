ae2fa63ba43765d1591cc8f3ce3427b83a120792
Ahmed Hazem <155444633+a7medhazem@users.noreply.github.com>
2026-02-15 23:49:44 +0200

    Add comments clarifying unique indexes in Voteconfigs

--- SurveyBasket.Api/Persistanse/EntitesConfigurations/VoteConfigurations.cs
+++ SurveyBasket.Api/Persistanse/EntitesConfigurations/VoteConfigurations.cs
@@ -4,6 +4,7 @@ public class VoteConfigurations : IEntityTypeConfiguration<Vote>
 {
     public void Configure(EntityTypeBuilder<Vote> builder)
     {
+        // has inedex PollId, UserId
         builder.HasIndex(x => new { x.PollId, x.UserId }).IsUnique();
     }
 }
