c6007e8cdda96f4382bf5b05c0cf5680b22e8318
Ahmed Hazem <155444633+a7medhazem@users.noreply.github.com>
2026-04-24 11:05:44 +0300

    Add user profile endpoint with service layer and DTO.
    - Create AccountController with /account/profile endpoint
    - Add UserProfileResponse DTO
    - Implement IUserService and UserService using ASP.NET Identity
    - Register IUserService in DI container

--- SurveyBasket.Api/DependencyInjection.cs
+++ SurveyBasket.Api/DependencyInjection.cs
@@ -47,6 +47,7 @@ public static class DependencyInjection
         services.AddScoped<IQuestionService, QuestionService>();
         services.AddScoped<IVoteService, VoteService>();
         services.AddScoped<IResultService, ResultService>();
+        services.AddScoped<IUserService, UserService>();
 
         // services.AddScoped<ICacheService, CacheService>();
 
